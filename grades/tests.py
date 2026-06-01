d74a726011bbd25d91c718c77745364988ceea0e
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-01 22:29:05 +0530

    Added failing test for grade A

--- grades/tests.py
+++ grades/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
-from .models import Student 
+from .models import Student
 
-# Create your tests here.
+class GradeTest(TestCase):
 
-class GradeTestCase(TestCase):
-    
     def test_grade_a(self):
-        student = Student(name='Shivam', obtained_marks=450, total_marks=500)
-        self.assertEqual(student.grade(), 'A')
+        student = Student(
+            name="Shivam",
+            obtained_marks=450,
+            total_marks=500
+        )
+
+        self.assertEqual(student.grade(), "A")
